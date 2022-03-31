e4166faf4bd9b7dd09e2b71ea1f60c86624f938d
Sean Stevens <sstevens@illinois.edu>
2022-03-31 13:26:43 -0500

    adjusted how default repo is added

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -4,56 +4,13 @@
 #
 # @example
 #   include gcsv5::repo
-class gcsv5::repo {
-  include epel
-
-  exec { 'RPM-GPG-KEY-Globus':
-    path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-    command => "wget -qO- ${gcsv5::release_url} | rpm2cpio - | cpio -i --quiet --to-stdout ./etc/pki/rpm-gpg/RPM-GPG-KEY-Globus > /etc/pki/rpm-gpg/RPM-GPG-KEY-Globus",
-    creates => '/etc/pki/rpm-gpg/RPM-GPG-KEY-Globus',
-    before  => Yumrepo['Globus-Toolkit']
+class gcsv5::repo (
+  Hash   $yumrepo
+) {
+  
+  $yumrepo_defaults = {
+    ensure  => present,
+    enabled => true,
   }
 
-  yumrepo { 'Globus-Toolkit':
-    descr          => 'Globus-Toolkit-6',
-    baseurl        => $gcsv5::repo_baseurl,
-    failovermethod => 'priority',
-    priority       => '98',
-    enabled        => '1',
-    gpgcheck       => '1',
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Globus',
-  }
-
-  yumrepo { 'Globus-Toolkit-6-Testing':
-    descr          => 'Globus-Toolkit-6-testing',
-    baseurl        => $gcsv5::repo_testing_baseurl,
-    failovermethod => 'priority',
-    priority       => '98',
-    enabled        => '0',
-    gpgcheck       => '1',
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Globus',
-  }
-
-  yumrepo { 'globus-connect-server-5':
-    descr          => 'Globus-Connect-Server-5',
-    baseurl        => $gcsv5::repo_baseurl_v5,
-    failovermethod => 'priority',
-    priority       => '98',
-    enabled        => '1',
-    gpgcheck       => '1',
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Globus',
-    require        => Exec['RPM-GPG-KEY-Globus'],
-  }
-
-  yumrepo { 'globus-connect-server-5-testing':
-    descr          => 'Globus-Connect-Server-5-Testing',
-    baseurl        => $gcsv5::repo_testing_baseurl_v5,
-    failovermethod => 'priority',
-    priority       => '98',
-    enabled        => '0',
-    gpgcheck       => '1',
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-Globus',
-    require        => Exec['RPM-GPG-KEY-Globus'],
-  }
-
-}
+  ensure_resources( 'yumrepo', $yumrepo, $yumrepo_defaults )
